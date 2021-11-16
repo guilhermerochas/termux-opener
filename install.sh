a40f1c0e7ace1644d7c753590c29e5630d56e4ea
Guilherme Rocha <59107569+guilhermerochas@users.noreply.github.com>
2021-11-16 02:22:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function main() {
   pip install youtube-dl requests
 
   mkdir -p "$HOME/.config/youtube-dl"
-  cat << EOF < $HOME/.config/youtube-dl/config
+  cat << EOF > $HOME/.config/youtube-dl/config
     --no-mtime
     -o /data/data/com.termux/files/home/storage/dcim/Youtube/%(title)s.%(ext)s
     -f "best[height<=480]"
